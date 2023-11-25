64646d488fca5ebf0044ebb205157ca90456f940
guybrushthreepwood1984 <riebenbauermichael@gmail.com>
2023-11-25 16:51:55 +0100

    added info to 06_1 about Module Contract in node

--- src/repositories/06_1_make-it-modular.js
+++ src/repositories/06_1_make-it-modular.js
@@ -13,3 +13,12 @@ workWithData.readListPrintFilteredFiles(
   process.argv[3],
   callback
 );
+
+/*
+Module Contract
+One important takeaway from this lesson apart from modularizing our code the node.js way, is the concept of a module contract.  In this case, our module contract is to export a single function that takes a directory, filter string, and callback as arguments.  The callback must be called once, with an error or data.  We are not modifying any global variables, and we are handling all errors by passing them to our callback to print an error message if an error occurs.
+
+This concept is important moving forward because modules created and published to npm (node.js package manager) should behave as intended for anyone expecting that the module will produce the results of the module contract.  Building with modules is what node.js all about.
+
+https://joecreager.com/learnyounode-lesson-6-make-it-modular/
+*/
